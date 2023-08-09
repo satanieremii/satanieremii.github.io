66c789f26862dffb35043cc2646ad91425f95733
satanieremii <124290838+satanieremii@users.noreply.github.com>
2023-08-09 13:44:20 +0200

    Add files via upload

---
+++ site/index1.js
@@ -0,0 +1,14 @@
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".imagegal");
+
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if ($(imagegal).position().top < pageBottom) {
+            $(imagegal).addClass("visible");
+        } else {
+            $(imagegal).removeClass("visible");
+        }
+    }
+});
